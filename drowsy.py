3870de05ef681e1584a223fb6305e3bf86983472
Niraj-Gandhi <62089845+Niraj-Gandhi@users.noreply.github.com>
2020-03-18 15:51:19 +0530

    Add files via upload

--- drowsy.py
+++ drowsy.py
@@ -1,5 +1,5 @@
 #Imports
-
+import tensorflow as tf
 import dlib
 from pathlib import Path
 import cv2
@@ -27,10 +27,25 @@ def eye_aspect_ratio(eye):
     ear=(X1+X2)/(2.0*Y)
     return ear
 
+def lip_dist(shape):
+    top_lip=shape[50:53]
+    top_lip=np.concatenate((top_lip,shape[61:64]))
+    lower_lip = shape[56:59]
+    lower_lip = np.concatenate((lower_lip, shape[65:68]))
+
+    mean_top=np.mean(top_lip,axis=0)
+    mean_low=np.mean(lower_lip,axis=0)
+
+    distance=abs(mean_top[1]-mean_low[1])
+    return distance
+
+def mouth_ear(mouth):
+    m_ear=dist.euclidean(mouth[0],mouth[3])
+    return m_ear
 eyethresh=0.3
 eyecons=48
 eyecons1=150
-
+yawn_thresh=25
 count=0
 alarm_status=False
 
@@ -40,26 +55,24 @@ predictor=dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 (lstart,lend)= face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rstart,rend)=face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+#(mstart,mend)=face_utils.FACIAL_LANDMARKS_68_IDXS["mouth"]
+
 c=cv2.VideoCapture(0)
 flag=0
 c2=0
-while True:
+start=0.0
+end=0.0
+check = 0
+eye_closure_duration = 0.0
+once=1
+drowsy_time=0
 
+
+while True:
     ret,frame=c.read()
     frame=imutils.resize(frame,width=450)
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     obj= detector(gray,0)
-    '''if obj==None:
-        print("AHBDJHGH")
-        count+=1
-        if count>=eyecons:
-            if not alarm_status:
-                alarm_status=True
-                t1=Thread(target=alarm(),daemon=True)
-                t.start()
-            cv2.putText(frame, "*****************ALERT****************", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0, 0, 255), 2)
-    count=0
-    alarm_status=False'''
     if obj:
         for s in obj:
             shape=predictor(gray,s)
@@ -71,13 +84,36 @@ while True:
             ear=(lear+rear)/2.0
             leftEyeHull=cv2.convexHull(leftEye)
             rightEyeHull=cv2.convexHull(rightEye)
+            '''
+            mouth=shape[mstart:mend]
+            h_mouth_ear=mouth_ear(mouth)
+            print(f"horizontal lip distance=={h_mouth_ear}")
+            '''
+            shape_m=predictor(gray,s)
+            x=shape_m.part(48).x
+            y=shape_m.part(48).y
+            #cv2.circle(frame,(x,y),3,(0,255,0),-1)
+            x1= shape_m.part(54).x
+            y1=shape_m.part(54).y
+            #cv2.circle(frame, (x1, y1), 3, (0, 255, 0), -1)
+            horizontal_mouth_dist=dist.euclidean((x,y),(x1,y1))
+            lip_of_distance=lip_dist(shape)
+            lip=shape[48:60]
+            lip=cv2.convexHull(lip)
             cv2.drawContours(frame,[leftEyeHull],-1,(255,255,255),1)
             cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
+            cv2.drawContours(frame,[lip],-1,(255,255,255),1)
+            mod_ear=float(round(ear,2))
+            cv2.putText(frame,f"EAR={mod_ear}",(0,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+            print(f"ear=={ear}")
+
             if ear<eyethresh:
+                check=1
+                if once==1:
+                    start = time.time()
+                    print(f"start time=={start}")
                 c2=0
                 count+=1
-                print(f"count=={count}")
-
                 if count>=eyecons:
                     if not alarm_status:
                         alarm_status=True
@@ -85,15 +121,37 @@ while True:
                         t.start()
                         print("*****************ALERT****************")
                     cv2.putText(frame,"*****************ALERT****************",(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+                once=0
             else:
+                if check==1:
+                    check=0
+                    once=1
+                    end=time.time()
+                    print(f"end time=={end}")
+                    eye_closure_duration = float(end-start)
+                    print(f"Eye Closure duarion =={eye_closure_duration}")
+                if eye_closure_duration>0.4 and eye_closure_duration<1.2:
+                    print("are you feeling drowsy")
+                    cv2.putText(frame, "are you feeling drowsy", (5, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.7, (0, 0, 255), 2)
                 count=0
                 alarm_status=False
+
+            if lip_of_distance>yawn_thresh and horizontal_mouth_dist<=41.2:
+                cv2.putText(frame,"are you feeling drowsy and tired",(7,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+                if not alarm_status:
+                    alarm_status=True
+                    t1=Thread(target=alarm,daemon=True)
+                    t1.start()
+            else:
+                alarm_status=False
+
+
 
 
     else:
         count=0
         c2+=1
-        print(f"c2=={c2}")
         if c2>=eyecons1:
             if not alarm_status:
                 alarm_status = True
@@ -104,6 +162,7 @@ while True:
                         (0, 0, 255), 2)
             c2=0
         alarm_status=False
+
 
     cv2.imshow("Frame",frame)
     key=cv2.waitKey(1) & 0xFF
