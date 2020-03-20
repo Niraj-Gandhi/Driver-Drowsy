8f7826d06fb68c55e031534fa4bc091fec98df82
Niraj-Gandhi <62089845+Niraj-Gandhi@users.noreply.github.com>
2020-03-20 20:30:38 +0530

    Add files via upload

--- drowsy.py
+++ drowsy.py
@@ -42,6 +42,42 @@ def lip_dist(shape):
 def mouth_ear(mouth):
     m_ear=dist.euclidean(mouth[0],mouth[3])
     return m_ear
+
+def get_gaze_ratio(eye_points,landmark):
+    eye_region = np.array([(landmark.part(eye_points[0]).x, landmark.part(eye_points[0]).y),
+                                (landmark.part(eye_points[1]).x, landmark.part(eye_points[1]).y),
+                                (landmark.part(eye_points[2]).x, landmark.part(eye_points[0]).y),
+                                (landmark.part(eye_points[3]).x, landmark.part(eye_points[3]).y),
+                                (landmark.part(eye_points[4]).x, landmark.part(eye_points[4]).y),
+                                (landmark.part(eye_points[5]).x, landmark.part(eye_points[5]).y)], np.int32)
+    height, width, _ = frame.shape
+    mask = np.zeros((height, width), np.uint8)
+    cv2.polylines(mask, [eye_region], True, 255, 2)
+    cv2.fillPoly(mask, [eye_region], 255)
+    eye = cv2.bitwise_and(gray, gray, mask=mask)
+    min_x = np.min(eye_region[:, 0])
+    max_x = np.max(eye_region[:, 0])
+    min_y = np.min(eye_region[:, 1])
+    max_y = np.max(eye_region[:, 1])
+
+    gray_eye = eye[min_y: max_y, min_x: max_x]
+    _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
+    thresh_height, thresh_width = threshold_eye.shape
+    left_threshold = threshold_eye[0:thresh_height, 0:int(thresh_width/2)]
+    left_white = cv2.countNonZero(left_threshold)
+    right_threshold = threshold_eye[0:thresh_height, int(thresh_width/2):width]
+    right_white = cv2.countNonZero(right_threshold)
+
+    if left_white == 0:
+        gaze_ratio = 1
+    elif right_white == 0:
+        gaze_ratio = 5
+    else:
+        gaze_ratio = left_white / right_white
+
+    return gaze_ratio
+
+
 eyethresh=0.3
 eyecons=48
 eyecons1=150
@@ -104,7 +140,7 @@ while True:
             cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
             cv2.drawContours(frame,[lip],-1,(255,255,255),1)
             mod_ear=float(round(ear,2))
-            cv2.putText(frame,f"EAR={mod_ear}",(0,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+            cv2.putText(frame,f"EAR={mod_ear}",(0,300),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
             print(f"ear=={ear}")
 
             if ear<eyethresh:
@@ -120,7 +156,7 @@ while True:
                         t = Thread(target=alarm, daemon=True)
                         t.start()
                         print("*****************ALERT****************")
-                    cv2.putText(frame,"*****************ALERT****************",(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+                    cv2.putText(frame,"ALERT",(100,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
                 once=0
             else:
                 if check==1:
@@ -139,13 +175,23 @@ while True:
 
             if lip_of_distance>yawn_thresh and horizontal_mouth_dist<=41.2:
                 cv2.putText(frame,"are you feeling drowsy and tired",(7,30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-                if not alarm_status:
+
+                '''if not alarm_status:
                     alarm_status=True
                     t1=Thread(target=alarm,daemon=True)
-                    t1.start()
+                    t1.start()'''
             else:
                 alarm_status=False
 
+            gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], shape_m)
+            gaze_ratio_right_eye = get_gaze_ratio([42, 43, 44, 45, 46, 47], shape_m)
+            gaze_ratio = (gaze_ratio_right_eye + gaze_ratio_left_eye) / 2
+            if gaze_ratio <= 1:
+                cv2.putText(frame, "RIGHT", (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            elif 1 < gaze_ratio < 1.7:
+                cv2.putText(frame, "CENTER", (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            else:
+                cv2.putText(frame, "LEFT", (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 
 
